c43f3333c8074eb7a7bd9830f637ee02b39d6947
OlshePete <olshevskii.pv@gmail.com>
2023-10-22 16:24:05 +0300

    fix style on commerce1

--- modules/commercial/service1/Service1PosterContent.tsx
+++ modules/commercial/service1/Service1PosterContent.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Box, Button, Heading, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Heading, Stack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 function Service1PosterContent() {
@@ -12,17 +12,22 @@ function Service1PosterContent() {
       flexDirection:'column',
       justifyContent:'space-between',
         padding:"70px",
-        borderRadius:"14px"
+        borderRadius:"14px",
+        gap:"16px" ,
+        height:'100%'
     }}
     >
-      <VStack alignItems={'flex-start'} pb="16px">
+      <VStack alignItems={'flex-start'}gap={0} maxW={'50%'}>
 
-      <Heading variant={'main_header'}>НАДУЕМ ваши</Heading>
-      <Heading variant={'sub_header'}>шары гелием</Heading>
+      <Heading variant={'main_header'} fontSize={'90px'} lineHeight={'90px'}>НАДУЕМ ваши</Heading>
+      <Heading variant={'sub_header'} fontSize={'63px'}  lineHeight={'63px'}>шары гелием</Heading>
       </VStack>
-      <Text variant={'banner_text'}>Вы можете наполнить у нас свои шары гелием
+      <Text variant={'banner_text'} fontSize={'16px'}  >Вы можете наполнить у нас свои шары гелием
 по низкой цене</Text>
+<Stack pt={"39px"}>
+
       <Button variant="solid" width={200} bg={"#F49AA5"}>подробнее</Button>
+</Stack>
     </Box>
   )
 }
