6fa62f0b211aa84093d1dec9e38c4e03cf3e3901
OlshePete <olshevskii.pv@gmail.com>
2024-09-08 17:09:35 +0300

    test api in docker 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const API_URL = process.env.API_URL | 'http://cms:1337'
 const nextConfig = {
+    output: "standalone",
     images: {
         remotePatterns: [{
                 protocol: 'https',
