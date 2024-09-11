d586b5c213c76e4131788656238d7e7793a832b2
OlshePete <olshevskii.pv@gmail.com>
2024-09-11 22:53:34 +0300

    test api in docker 6

--- services/getMetaData.ts
+++ services/getMetaData.ts
@@ -7,14 +7,28 @@ const headers = {
 }
 
  const getStartPageMetaDate = async () => {
+   console.log(API_TOKEN);
+   
     const response = await fetch(`http://cms:1337/api/web-setting`, {
         headers,
         next:{
             revalidate:60,
         }
     });
+    const response2 = await fetch(`http://cms:1337/cms/api/web-setting`, {
+        headers,
+        next:{
+            revalidate:60,
+        }
+    });
+    const response3 = await fetch(`http://cms:1337/cms/api/web-settings`, {
+        headers,
+        next:{
+            revalidate:60,
+        }
+    });
   
-    if (!response.ok) throw new Error(`Ошибка при загрузке мета информации. ${JSON.stringify(response)}`);
+    if (!response.ok) throw new Error(`Ошибка при загрузке мета информации. ${JSON.stringify({1:response.status, 2:response2.status,3:response3.status, })}`);
     
     const data:IWebMeta = await response.json()
     console.log("Мета информация успешно получена!", data);
