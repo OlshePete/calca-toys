ce93431b7455a3ce8ec589bbb438671f49c196a1
OlshePete <olshevskii.pv@gmail.com>
2024-09-16 22:10:56 +0300

    test client side api request 3

--- components/test/TestApi.tsx
+++ components/test/TestApi.tsx
@@ -6,17 +6,20 @@ import React, { useEffect, useState } from 'react'
 function TestApi() {
     const [data, setData] = useState<IWebMeta | null>(null)
     const {API_URL} =  process.env
-    useEffect(() => {
-        async function getData(cld:(data:IWebMeta)=>void) {
-            const res = await getStartPageMetaDate()
-            cld(res)
-        }
+    async function getData(cld:(data:IWebMeta)=>void) {
+        const res = await getStartPageMetaDate()
+        cld(res)
+    }
+    const handlerequest = () => {
         setData && getData(setData)
-    }, [setData])
-    console.log("API_URL client",API_URL)
+        console.log("hanlder process.env client",process.env)
+    }
+
+    console.log("process.env client",process.env)
   return (
     <div>
         <h1 style={{marginTop:'5rem'}}>API test</h1>
+        <button  type='button' onClick={handlerequest}>fetch</button>
         {
             data && <p>{JSON.stringify(data)}</p>
         }
